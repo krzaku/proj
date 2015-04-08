0c2cb9227c4c0972c9dfb061a4416ca2951214f8
krzaku <tzaleski@gmail.com>
2015-04-08 17:24:58 +0200

    added full paths for script commands

--- puppet/files/setup_host_net.sh
+++ puppet/files/setup_host_net.sh
@@ -4,18 +4,18 @@
 #Date: 2015-04-09 23:00
 #Ver: 1.0.0
 
-ovs-vsctl add-br rbridge
-#ifconfig rbridge up
-ovs-vsctl add-port rbridge eth0
-ifconfig eth0 0
-ifconfig rbridge 192.168.233.100 netmask 255.255.255.0 up
-route add default gw 192.168.233.2
-ip tuntap add mode tap vport1
-ip tuntap add mode tap vport2
-ip tuntap add mode tap vport3
-ifconfig vport1 up
-ifconfig vport2 up
-ifconfig vport3 up
-ovs-vsctl add-port rbridge vport1
-ovs-vsctl add-port rbridge vport2
-ovs-vsctl add-port rbridge vport3
+/usr/bin/ovs-vsctl add-br rbridge
+/sbin/ifconfig rbridge up
+/usr/bin/ovs-vsctl add-port rbridge eth0
+/sbin/ifconfig eth0 0
+/sbin/ifconfig rbridge 192.168.233.100 netmask 255.255.255.0
+/sbin/route add default gw 192.168.233.2
+/sbin/ip tuntap add mode tap vport1
+/sbin/ip tuntap add mode tap vport2
+/sbin/ip tuntap add mode tap vport3
+/sbin/ifconfig vport1 up
+/sbin/ifconfig vport2 up
+/sbin/ifconfig vport3 up
+/usr/bin/ovs-vsctl add-port rbridge vport1
+/usr/bin/ovs-vsctl add-port rbridge vport2
+/usr/bin/ovs-vsctl add-port rbridge vport3
