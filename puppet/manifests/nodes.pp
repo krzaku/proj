c6897acc025707a66b50e48444c7d629e9c76a96
krzaku <tzaleski@gmail.com>
2015-04-08 16:02:48 +0200

    merge manifest for u01

--- puppet/manifests/nodes.pp
+++ puppet/manifests/nodes.pp
@@ -1,5 +1,11 @@
 node basenode {
 
+  exec { "apt_get_update":
+    command => "/usr/bin/apt-get -y update"
+  }
+
+  Exec["apt_get_update"] -> Package <| |>
+
   exec { "download_riakcs":
     command => "/usr/bin/wget 192.168.233.209/riak-cs_2.0.0-1_amd64.deb -O /tmp/riak-cs_2.0.0-1_amd64.deb",
 #    command => "/usr/bin/wget http://s3.amazonaws.com/downloads.basho.com/riak-cs/2.0/2.0.0/ubuntu/trusty/riak-cs_2.0.0-1_amd64.deb -O /tmp/riak-cs_2.0.0-1_amd64.deb",
@@ -17,6 +23,36 @@ node basenode {
                 ]
   }
 
+}
+
+node basehost {
+
+  exec { "apt_get_update":
+    command => "/usr/bin/apt-get -y update"
+  }
+
+  Exec["apt_get_update"] -> Package <| |>
+
+  $host_packages = [
+                      "git",
+                      "openvswitch-switch",
+                      "virtualbox",
+                      "vagrant",
+                      "vim",
+                    ]
+  package { $host_packages:
+    ensure => installed,
+  }
+
+  exec { "vagrant_providers":
+    user => "root",
+    path => "/usr/bin:/usr/sbin:/bin",
+    environment => "HOME=/root",
+    command => "/usr/bin/vagrant plugin install vagrant-vbguest",
+    require => [
+                  Package["vagrant"],
+               ],
+  }
 }
 
 import 'nodes/*.pp'
